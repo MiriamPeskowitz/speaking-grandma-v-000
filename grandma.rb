2c58f71719344508be99910ca6d788f447836c57
Miriam Peskowitz <miriam5000@gmail.com>
2017-09-19 16:22:56 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,13 @@
 def speak_to_grandma(phrase)
 
 
-  if  phrase != phrase.upcase
+  if  phrase !== phrase.upcase
       return "HUH?! SPEAK UP, SONNY!"
 
-  elsif phrase = "I LOVE YOU GRANDMA!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN!"
 
-  elsif phrase = phrase.upcase and phrase != "I LOVE YOU GRANDMA!" 
+  elsif phrase == phrase.upcase and phrase !== "I LOVE YOU GRANDMA!" 
       return "NO, NOT SINCE 1938!"
 
 
